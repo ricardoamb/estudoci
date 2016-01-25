6bb97cc01885a1a93ecb98d937475cb8d7739814
Ricardo Amb <ricardoamb.dev@gmail.com>
2016-01-25 17:51:43 -0200

    Form criado

--- src/app/controllers/crud.php
+++ src/app/controllers/crud.php
@@ -7,7 +7,7 @@ class Crud extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper('url');
+        $this->load->helper(array('url','form'));
     }
 
     public function index()
--- src/app/views/telas/create.php
+++ src/app/views/telas/create.php
@@ -1,2 +1,17 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-echo basename(__FILE__);
+
+echo form_open('crud/create'); // Abertura do form
+
+    echo form_label('Nome Completo: ');
+    echo form_input(array('id'=>'nome','class'=>'input-text','name'=>'nome'),'','autofocus');
+    echo form_label('Email: ');
+    echo form_input(array('id'=>'email','class'=>'input-text','name'=>'email'));
+    echo form_label('Login: ');
+    echo form_input(array('id'=>'login','class'=>'input-text','name'=>'login'));
+    echo form_label('Senha: ');
+    echo form_password(array('id'=>'password','class'=>'input-text','name'=>'password'));
+    echo form_label('Repita a Senha: ');
+    echo form_password(array('id'=>'password2','class'=>'input-text','name'=>'password2'));
+    echo form_submit(array('name'=>'cadastrat','value'=>'Cadastrar'));
+
+echo form_close(); // Fechamento do form
